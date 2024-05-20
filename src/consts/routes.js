a02314195f86157f84a6ce90acc87f1558601022
Lapitskiy <hiimisha@mail.ru>
2024-05-21 02:13:21 +0300

    лишнее

--- src/consts/routes.js
+++ src/consts/routes.js
@@ -6,8 +6,3 @@ export const routes = [
   { path: Paths.ABOUT, element: <About />, name: 'О приложении' },
   { path: Paths.QUOTES, element: <Quotes />, name: 'Котировки' },
 ];
-
-// цветовая палитра
-// #d34166 - красный
-// #65598d - фиолетовый
-
